0c9fa7cacbc80f13556a602cbe0315ac695fc1d0
Alex Duzi <duzihd@gmail.com>
2026-08-22 15:32:50 -0300

    adding integration tests

---
+++ src/test/java/com/alexduzi/movies/controller/MovieControllerIT.java
@@ -0,0 +1,48 @@
+package com.alexduzi.movies.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class MovieControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldCalculateProducerIntervalsSuccessfullyWithAnyDataset() throws Exception {
+        mockMvc.perform(get("/api/v1/movie/producer-intervals")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.min").isArray())
+                .andExpect(jsonPath("$.max").isArray())
+                .andExpect(jsonPath("$.min[0].producer").exists())
+                .andExpect(jsonPath("$.min[0].interval").isNumber())
+                .andExpect(jsonPath("$.min[0].previousWin").isNumber())
+                .andExpect(jsonPath("$.min[0].followingWin").isNumber())
+                .andExpect(jsonPath("$.max[0].producer").exists())
+                .andExpect(jsonPath("$.max[0].interval").isNumber())
+                .andExpect(jsonPath("$.max[0].previousWin").isNumber())
+                .andExpect(jsonPath("$.max[0].followingWin").isNumber());
+    }
+
+    @Test
+    void shouldMatchExpectedValuesForDefaultDataset() throws Exception {
+        mockMvc.perform(get("/api/v1/movie/producer-intervals")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.min[0].producer").value("Joel Silver"))
+                .andExpect(jsonPath("$.min[0].interval").value(1))
+                .andExpect(jsonPath("$.max[0].producer").value("Matthew Vaughn"))
+                .andExpect(jsonPath("$.max[0].interval").value(13));
+    }
+}
